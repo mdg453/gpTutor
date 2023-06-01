4a8eb56db0eda8e849942c06febf93cb657e51ad
meitar453 <meitar.degracea@mail.huji.ac.il>
2023-06-01 05:39:38 +0300

    v0.6 test

--- login_page/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def login(request):
-    template = "main/login.html"
-    return render(request, template)
--- users/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def users(request):
-    template = "main/users.html"
-    return render(request, template)
